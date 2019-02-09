e58171121b162925859c49b339a822438245e310
yasharzb <yasharzb@ce.sharif.edu>
2019-02-09 22:29:11 +0330

    Random Moves

--- src/client/AI.java
+++ src/client/AI.java
@@ -3,6 +3,7 @@ package client;
 import client.model.*;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class AI
 {
@@ -19,15 +20,92 @@ public class AI
     {
         //System.out.println("pre process started");
     }
-
     public void pickTurn(World world)
     {
+        Random random=new Random();
         //System.out.println("pick started");
-
+        world.pickHero(HeroName.values()[random.nextInt(4)]);
+        for(Hero hero1:world.getMyHeroes())
+            System.out.println(hero1.getName());
     }
 
     public void moveTurn(World world)
     {
+        draw_map(world);
+        Random random=new Random();
+        int rand;
+        Hero hero;
+        boolean check_hero=false,moved=false;
+        hero=world.getMyHeroes()[random.nextInt(4)];
+        while (!moved){
+            rand=random.nextInt(4);
+            switch (rand){
+                case 0:
+                    while (!world.getMap().getCell(hero.getCurrentCell().getRow()-1,hero.getCurrentCell().getColumn())
+                            .isWall()){
+                        for(Hero my_hero:world.getMyHeroes()){
+                            if(world.getMap().getCell(hero.getCurrentCell().getRow()-1,hero.getCurrentCell().getColumn())
+                                    .equals(my_hero)){
+                                check_hero=true;
+                            }
+                        }
+                        if(!check_hero){
+                            world.moveHero(hero,Direction.values()[rand]);
+                            moved=true;
+                        }
+                    }break;
+                case 1:
+                    if(!world.getMap().getCell(hero.getCurrentCell().getRow()+1,hero.getCurrentCell().getColumn())
+                            .isWall()){
+                        for(Hero my_hero:world.getMyHeroes()){
+                            if(world.getMap().getCell(hero.getCurrentCell().getRow()+1,hero.getCurrentCell().getColumn())
+                                    .equals(my_hero)){
+                                check_hero=true;
+                            }
+                        }
+                        if(!check_hero){
+                            world.moveHero(hero,Direction.values()[rand]);
+                            moved=true;
+                        }
+                    }break;
+                case 2:
+                    if(!world.getMap().getCell(hero.getCurrentCell().getRow(),hero.getCurrentCell().getColumn()-1)
+                            .isWall()){
+                        for(Hero my_hero:world.getMyHeroes()){
+                            if(world.getMap().getCell(hero.getCurrentCell().getRow(),hero.getCurrentCell().getColumn()-1)
+                                    .equals(my_hero)){
+                                check_hero=true;
+                            }
+                        }
+                        if(!check_hero){
+                            world.moveHero(hero,Direction.values()[rand]);
+                            moved=true;
+                        }
+                    }break;
+                case 3:
+                    if(!world.getMap().getCell(hero.getCurrentCell().getRow(),hero.getCurrentCell().getColumn()+1)
+                            .isWall()){
+                        for(Hero my_hero:world.getMyHeroes()){
+                            if(world.getMap().getCell(hero.getCurrentCell().getRow(),hero.getCurrentCell().getColumn()+1)
+                                    .equals(my_hero)){
+                                check_hero=true;
+                            }
+                        }
+                        if(!check_hero){
+                            world.moveHero(hero,Direction.values()[rand]);
+                            moved=true;
+                        }
+                    }break;
+            }
+        }
+    }
+
+    public void actionTurn(World world) {
+        //System.out.println("action started");
+
+    }
+
+    public void draw_map(World world){
         boolean[][] check=new boolean[world.getMap().getRowNum()][world.getMap().getColumnNum()];
         for (int i = 0; i <  world.getMap().getRowNum(); i++) {
             for (int j = 0; j < world.getMap().getColumnNum(); j++) {
@@ -37,7 +115,7 @@ public class AI
                 if (!world.getMap().getCell(i, j).isWall()) {
                     for(Hero hero: world.getMyHeroes()){
                         if(hero.getCurrentCell().equals(world.getMap().getCell(i,j))){
-                            System.out.print(ANSI_GREEN+hero.getId()+ANSI_RESET);
+                            System.out.print(ANSI_GREEN+hero.getId()+" "+ANSI_RESET);
                             check[i][j]=true;
                         }
                     }
@@ -64,11 +142,6 @@ public class AI
             }
             System.out.println();
         }
-    }
-
-    public void actionTurn(World world) {
-        //System.out.println("action started");
-
     }
 
 }
